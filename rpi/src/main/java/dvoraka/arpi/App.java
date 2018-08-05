5728b9afb8c59d21338253d46bf6ef907336d891
dvoraka <alen.dvorak@gmail.com>
2018-08-05 17:10:54 +0200

    Prepare app prototype

--- rpi/src/main/java/dvoraka/arpi/App.java
+++ rpi/src/main/java/dvoraka/arpi/App.java
@@ -1,9 +1,12 @@
 package dvoraka.arpi;
 
+import gnu.io.CommPortIdentifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Enumeration;
 
 @SpringBootApplication
 public class App {
@@ -16,6 +19,14 @@ public class App {
     public CommandLineRunner commandLineRunner() {
         return args -> {
             System.out.println("App");
+
+            Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+            while (portEnum.hasMoreElements()) {
+                CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+                System.out.println("P: " + currPortId.getName());
+            }
+
+            System.out.println();
         };
     }
 }
